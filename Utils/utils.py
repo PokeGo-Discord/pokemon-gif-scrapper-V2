66858c727acd1b68333e11806bb84b7f0e256116
KevinDavai <kevin.davailles@etu.uca.fr>
2022-07-06 15:39:52 +0200

    Better gif

--- Utils/utils.py
+++ Utils/utils.py
@@ -8,8 +8,13 @@ FILE: utils.py
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
+import unicodedata
 
 BASE_URL = 'https://www.professorlotus.com/'
+
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
 
 def getImgUrl(pokemonName):
     url = "https://www.professorlotus.com/index.php?Type1=Bug&Type2=&search=" + pokemonName + "&HighDataOpt=on"
@@ -19,6 +24,28 @@ def getImgUrl(pokemonName):
     url = BASE_URL + img[0]['src']
     return url
 
+def getPokemonNamesFR_URL():
+    pokemonUrlFR=[]
+    for i in range(1,9):
+        url = "https://pokeapi.co/api/v2/generation/" + str(i) + "/"
+        r = requests.get(url)
+        data = r.json()
+        for i in data['pokemon_species']:
+            print(i['url'])
+            pokemonUrlFR.append(i['url'])
+    
+    return pokemonUrlFR
+
+def getPokemonNamesFR(pokemonUrlFR):
+    pokemonNamesFR=[]
+    for url in pokemonUrlFR:
+        r = requests.get(url)
+        data = r.json()
+        pokemonNamesFR.append(data['names'][4]['name'])
+        print(data['names'][4]['name'])
+        
+    return pokemonNamesFR
+        
 # Retrieve All Pokemon Names By Generation Id
 def getPokemonNames():
     pokemonNames=[]
